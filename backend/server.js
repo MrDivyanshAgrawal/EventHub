1de10b9d26f5c1cc02e4867b3d5012abc2404bd5
Divyansh Agrawal <divyansh1001agrawal@gmail.com>
2025-09-27 14:52:41 +0530

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use("/api/payments", paymentRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/:path", (req, res) => {
     res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"));
   });
 } else {
